db3763b24e56c3e204dbd236811738145ba8662b
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2019-10-29 19:37:54 -0300

    :sparkles: Introduce new random enemy spawner

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Planet planet;
+
+    bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateOrbitMovement(){
+        float dist = Vector3.Distance(planet.transform.position, transform.position);
+        if (dist < 1) dist = 1;
+
+        Vector3 gravDirection = (transform.position - planet.transform.position).normalized;
+        Vector3 gravPull = gravDirection * (-planet.gravity/dist);
+
+        GetComponent<Rigidbody>().AddForce(gravPull);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGrounded){
+            UpdateOrbitMovement();
+        }else{
+            //  UpdateGroundedMovement();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Planet planet;
+
+    public GameObject enemyPrefab;
+
+    GameObject enemyGroup;
+
+    [Range(0,20)] public float secondsSpawnInterval = 2;
+    float secondsFromlastInterval = 0;
+
+    private void ResetSpawnerPosition(){
+        transform.RotateAround(
+            planet.transform.position, 
+            new Vector3(Random.Range(0,10),Random.Range(0,10),Random.Range(0,10)), 
+            Random.Range(0,360)
+        );
+    }
+
+    private void CreateEnemyGroupOnEditor(){
+        enemyGroup = new GameObject();
+        enemyGroup.name = "Enemy Group";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ResetSpawnerPosition();
+        CreateEnemyGroupOnEditor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (secondsFromlastInterval >= secondsSpawnInterval){
+            SpawnEnemyInOrbit();
+            secondsFromlastInterval = 0;
+            ResetSpawnerPosition();
+        }else{
+            secondsFromlastInterval += Time.deltaTime;
+        }
+    }
+
+    private void SpawnEnemyInOrbit(){
+        GameObject newEnemy = Instantiate(enemyPrefab, this.transform.position, Quaternion.identity);
+        newEnemy.transform.Rotate(this.transform.forward, 180);
+        newEnemy.transform.parent = enemyGroup.transform;
+    }
+}
